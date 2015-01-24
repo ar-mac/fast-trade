addff38bfb879d6c340acb73229fae0d72833db3
pokiujf <arkadiusz.machalica@gmail.com>
2015-01-24 14:05:18 +0000

    Incorrect user is now in setup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,60 +5,58 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = users(:tom)
     @admin = users(:admin)
+    @user2 = User.new(name: 'Mark999',
+                      password: 'asdfasdf',
+                      password_confirmation: 'asdfasdf',
+                      region: 'Łódzkie')
   end
   
   test 'name validations' do
     
-    user = User.new(  password: 'asdfasdf',
-                      password_confirmation: 'asdfasdf',
-                      region: 'Łódzkie')
-    assert_not user.valid?, 'user name is blank'
+    @user2.update(name: '')
+    assert_not @user2.valid?, 'user name is blank'
     
-    user.update(name: @user.name)
-    assert_not user.valid?, 'user name is not unique'
+    @user2.update(name: @user.name)
+    assert_not @user2.valid?, 'user name is not unique'
     
-    user.update(name: 'aa')
-    assert_not user.valid?, 'user name is less than 3 letters'
+    @user2.update(name: 'aa')
+    assert_not @user2.valid?, 'user name is less than 3 letters'
     
-    user.update(name: 'a' * 21 )
-    assert_not user.valid?, 'user name is more than 20 letters'
+    @user2.update(name: 'a' * 21 )
+    assert_not @user2.valid?, 'user name is more than 20 letters'
     
-    user.update(name: 'Mark999')
-    assert user.valid?, 'user name is correct'
-    assert user.active?, 'user is inactive'
-    assert_not user.admin?, 'user is admin'
+    @user2.update(name: 'Mark999')
+    assert @user2.valid?, 'user name is correct'
+    assert @user2.active?, 'user is inactive'
+    assert_not @user2.admin?, 'user is admin'
   end
   
   test 'region validations' do
     
-    user = User.new( name: 'Mark999',
-                      password: 'asdfasdf',
-                      password_confirmation: 'asdfasdf')
     User::REGIONS.each do |reg|
-      user.update(region: reg)
-      assert user.valid?, "#{reg} is invalid"
+      @user2.update(region: reg)
+      assert @user2.valid?, "#{reg} is invalid"
     end
     
     incorrect_regions = ['slask', 'mazowsze', 'washington', 'łódź', 'łódzkie']
     incorrect_regions.each do |reg|
-      user.update(region: reg)
-      assert_not user.valid?, "#{reg} is valid"
+      @user2.update(region: reg)
+      assert_not @user2.valid?, "#{reg} is valid"
     end
+    
   end
   
   test 'password validations' do
     
-    user = User.new( name: 'Mark999',
-                      password: 'asdfasdf',
-                      password_confirmation: 'qwerqwer',
-                      region: 'Łódzkie')
-    assert_not user.valid?, 'wrong password confirmation'
+    @user2.update( password: 'asdfasdf',
+                  password_confirmation: 'qwerqwer')
+    assert_not @user2.valid?, 'wrong password confirmation'
     
-    user.update(password: '', password_confirmation: '')
-    assert_not user.valid?, 'passwords empty'
+    @user2.update(password: '', password_confirmation: '')
+    assert_not @user2.valid?, 'passwords empty'
     
-    user.update(password: 'asdfas', password_confirmation: 'asdfas')
-    assert_not user.valid?, 'password too short'
+    @user2.update(password: 'asdfas', password_confirmation: 'asdfas')
+    assert_not @user2.valid?, 'password too short'
   end
   
 end
