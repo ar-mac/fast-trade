ad251805cd890b050cf8755bd35fa913d0676343
pokiujf <arkadiusz.machalica@gmail.com>
2015-01-25 22:24:21 +0000

    TEMPORARY login logout implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+  
+  #all code below will go to SessionsController
+    
+    def log_in(user)
+      session[:user_id] = user.id
+    end
+    
+    def remember(user)
+      cookies.permament.signed[:user_id] = user.id
+      log_in(user)
+    end
+    
+    def log_out(user)
+      session.delete(:user_id)
+      forget(user)
+    end
+    
+    def forget(user)
+      cookies.delete(:user_id)
+    end
+    
+    def logged_in?
+      !current_user.nil?
+    end
+    
+    def current_user
+      u_id = session[:user_id] || cookies.signed[:user_id]
+      current = User.find_by(id: u_id)
+      log_in(current) if !current.nil?
+    end
+  
 end
