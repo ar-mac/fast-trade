646df1a21fedd7f8414b2f99a48349610ab3c64f
pokiujf <arkadiusz.machalica@gmail.com>
2015-01-25 02:15:28 +0000

    Adds static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root 'users#index'
+  root 'statics#home'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
