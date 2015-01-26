4fe14c2bae1af78b5ccb0b068b30d1f0363438ee
pokiujf <arkadiusz.machalica@gmail.com>
2015-01-26 05:05:27 +0000

    Pagination options for inner and outer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
   
+  def paginate(collection, options = {})
+    defaults = {
+      inner_window: 2,
+      outer_window: 1
+    }
+    options = defaults.merge(options)
+  
+    will_paginate collection, options
+  end
+  
 end
