3f99729b0696978efa44e9628e813cef2fbda8f0
pokiujf <arkadiusz.machalica@gmail.com>
2015-01-28 02:53:12 +0000

    Refactoring logic in user forms

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  
+  def password_fields
+    if @current_user == @user
+      render partial: 'users/form_fields', locals: {edit: true}
+    elsif @current_user.nil?
+      render partial: 'users/form_fields', locals: {edit: false}
+    else
+      
+    end
+  end
+  
 end
