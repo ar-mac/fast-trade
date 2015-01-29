f4547d8d0e8a62e44feb838c3c9f3d3329fbf9c2
pokiujf <arkadiusz.machalica@gmail.com>
2015-01-29 00:16:14 +0000

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 
+# users creation
 User.create(name: 'Beziq',
   password: 'asdfasdf',
   password_confirmation: 'asdfasdf',
@@ -6,11 +7,29 @@ User.create(name: 'Beziq',
   active: true,
   admin: true)
   
-32.times do |n| 
+50.times do |n| 
   User.create(name: Faker::Name.name,
   password: 'asdfasdf',
   password_confirmation: 'asdfasdf',
   region: User::REGIONS[n % 16],
   active: true,
   admin: false)
+end
+
+# categories creation
+Category::NAMES.each_index do |i|
+  Category.create(name_id: i )
+end
+
+# offers creation
+User.all.each do |user|
+  50.times do |n|
+    user.offers.create(
+      title: "#{Faker::Commerce.product_name} #{Faker::Lorem.word}",
+      content: Faker::Lorem.sentence(rand(3..9)),
+      valid_until: (Time.zone.today + rand(1..90).days),
+      status_id: ( n % 3 ),
+      category_id: rand(0..11)
+      )
+  end
 end
