a368879f67b40f22b75fca3c078c0f406f7b42da
pokiujf <arkadiusz.machalica@gmail.com>
2015-01-31 08:01:21 +0000

    Pagination defaults

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,3 @@
 module ApplicationHelper
   
-  def paginate(collection, options = {})
-    defaults = {
-      inner_window: 2,
-      outer_window: 1
-    }
-    options = defaults.merge(options)
-  
-    will_paginate collection, options
-  end
-  
 end
