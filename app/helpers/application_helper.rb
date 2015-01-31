1098d10f783ceb30ea413a497d40ac976993d0b0
pokiujf <arkadiusz.machalica@gmail.com>
2015-01-31 14:44:19 +0000

    Flash with font awesome and user form debug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   
+  def fa_for_msg(msg_type)
+    case msg_type
+    when 'success' then raw("<i class='fa fa-check fa-lg'></i>")
+    when 'info' then raw("<i class='fa fa-info fa-lg'></i>")
+    when 'danger' then raw("<i class='fa fa-exclamation fa-lg'></i>")
+    end
+  end
+  
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,5 @@
 module UsersHelper
   
-  def password_fields
-    if @current_user == @user
-      render partial: 'users/form_fields', locals: {edit: true}
-    elsif @current_user.nil?
-      render partial: 'users/form_fields', locals: {edit: false}
-    else
-      
-    end
-  end
-  
   def offer_status_nav
     if current_user? || admin?
       render 'status_nav'
