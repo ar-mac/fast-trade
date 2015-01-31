d2746ac06c4e9bc689374f790a656992b455f95b
pokiujf <arkadiusz.machalica@gmail.com>
2015-01-31 13:57:39 +0000

    proper links for header navigation

--- test/integration/homepage_test.rb
+++ test/integration/homepage_test.rb
@@ -10,41 +10,39 @@ class HomepageTest < ActionDispatch::IntegrationTest
   test 'links for non logged in person' do
     get root_path
     assert_template 'statics/home'
-    # assert_select "a[href = ?], login_path, count: 2"
-    # assert_select "a[href = ?], register_path, count: 2"
-    # assert_select "a[href = ?], offers_path, count: 2"
+    assert_select "a[href = ?]", login_path, count: 2
+    assert_select "a[href = ?]", new_user_path, count: 2
+    assert_select "a[href = ?]", offers_path, count: 1
   end
   
   test 'links for logged in user' do
-    # log_in(@user)
-    # get root_path
-    # assert_select "a[href = ?], login_path, count: 1"
-    # assert_select "a[href = ?], register_path, count: 1"
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href = ?]", login_path, count: 1
+    assert_select "a[href = ?]", new_user_path, count: 1
     
-    # assert_select "a[href = ?], user_path(@user), count: 1"
-    # assert_select "a[href = ?], edit_user_path(@user), count: 1"
-    # assert_select "a[href = ?], user_offers_path(@user), count: 1"
-    # assert_select "a[href = ?], new_offer_path(@user), count: 1"
+    assert_select "a[href = ?]", user_path(@user), count: 1
+    assert_select "a[href = ?]", edit_user_path(@user), count: 1
+    assert_select "a[href = ?]", new_offer_path, count: 1
     
-    # assert_select "a[href = ?], logout_path, count: 1"
+    assert_select "a[href = ?]", logout_path, count: 1
     
   end
   
   test 'link for admin user' do
-    # log_in(@user)
-    # get root_path
-    # assert_select "a[href = ?], login_path, count: 1"
-    # assert_select "a[href = ?], register_path, count: 1"
+    log_in_as(@admin)
+    get root_path
+    assert_select "a[href = ?]", login_path, count: 1
+    assert_select "a[href = ?]", new_user_path, count: 1
     
-    # assert_select "a[href = ?], user_path(@user), count: 1"
-    # assert_select "a[href = ?], edit_user_path(@user), count: 1"
-    # assert_select "a[href = ?], user_offers_path(@user), count: 1"
-    # assert_select "a[href = ?], new_offer_path(@user), count: 1"
-    # assert_select "a[href = ?], logout_path, count: 1"
+    assert_select "a[href = ?]", user_path(@admin), count: 1
+    assert_select "a[href = ?]", edit_user_path(@admin), count: 1
+    assert_select "a[href = ?]", new_offer_path, count: 1
+    assert_select "a[href = ?]", logout_path, count: 1
     
-    # assert_select "a[href = ?], admin_users_path, count: 1"
-    # assert_select "a[href = ?], admin_offers_path, count: 1"
-    # assert_select "a[href = ?], admin_offers_path(status: 0), count: 1"
+    assert_select "a[href = ?]", admin_users_path, count: 1
+    assert_select "a[href = ?]", offers_path, count: 3
+    assert_select "a[href = ?]", offers_path(status: 0), count: 1
   end
   
   test 'existence of search form' do
